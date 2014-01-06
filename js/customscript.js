7bd6b0664b60cb1cdbe1ad53bb1076ee3163314c
alvsroa <alvsroa@outlook.com>
2014-01-06 10:55:52 -0800

    custom css commit

---
+++ js/customscript.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+ 
+/*---Activate Plug-ins----*/
+
+/************Tooltips***************/
+$('.facebook-tooltip').tooltip();
+$('.google-plus-tooltip').tooltip();
+$('.youtube-tooltip').tooltip();
+
+/************Carousel***************/
+$('.carousel').carousel({
+		interval: 5000
+	  })
